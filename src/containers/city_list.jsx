df60d488fa05267fea36a852dcd233df0eca46c4
Ryan Mansfield <ryanmansfield86@gmail.com>
2019-11-22 14:03:47 -0600

    completed city list container

--- src/containers/city_list.jsx
+++ src/containers/city_list.jsx
@@ -1,13 +1,31 @@
 import React, { Component }from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import City from '../containers/city';
 
 class CityList extends Component {
+  renderList () {
+    return this.props.cities.map((city) => {
+      return (
+        <City key={city.name} city={city} />
+      );
+    });
+  };
+
   render () {
     return (
-      <div>
-        // TODO
-      </div>
-    )
+      <ul classname="list-group cities"
+        {this.renderList()}
+      </ul>
+    );
   }
+};
+
+function mapStateToProps(state) {
+  return {
+    cities: state.cities
+  };
 }
 
-export default CityList;
+export default connect(mapStateToProps)(CityList);
